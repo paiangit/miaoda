731de4a26624c45bdd86fe019501af25e5bed4a6
paiangit <pai_an@outlook.com>
2022-01-30 23:22:53 +0800

    revert: 回滚错误修改

--- craco.config.js
+++ craco.config.js
@@ -1,23 +1,23 @@
-const CracoLessPlugin = require("craco-less");
-const path = require("path");
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
 
 module.exports = {
   webpack: {
     alias: {
-      "~": path.resolve(__dirname, "./src/"),
+      '~': path.resolve(__dirname, './src/'),
     },
     configure: {
-      entry: path.resolve(__dirname, "./src/index.tsx"),
+      entry: path.resolve(__dirname, './src/index.tsx'),
     },
   },
   jest: {
     configure: {
       moduleNameMapper: {
-        "^~(.*)$": "<rootDir>/src$1",
+        '^~(.*)$': '<rootDir>/src$1',
       },
-      roots: ["<rootDir>/src", "<rootDir>/test"],
-      testMatch: ["<rootDir>/test/**/*.{spec,test}.{js,jsx,ts,tsx}"],
-      setupFilesAfterEnv: "<rootDir>/test/setupTests.ts",
+      roots: ['<rootDir>/src', '<rootDir>/test'],
+      testMatch: ['<rootDir>/test/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+      setupFilesAfterEnv: '<rootDir>/test/setupTests.ts',
     },
   },
   plugins: [
