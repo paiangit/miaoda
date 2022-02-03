d7d3569e850314e22d9a45ffcd6f366f82b6ec28
paiangit <pai_an@outlook.com>
2022-02-04 02:53:07 +0800

    feat: 添加登录功能

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { Button, message } from 'antd';
+import { Form, Button, message, Input } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.less';
 import api from './api.ts';
@@ -8,57 +8,68 @@ export default function LoginPage() {
   const loginFormRef = useRef();
   const navigate = useNavigate();
 
-  function clickHandler() {
-    return (e) => {
-      e.preventDefault();
+  const handleFinish = (values) => {
+    api.login(values).then((res) => {
+      if (res.code === 0) {
+        let { token, id, username } = res.data;
+        if (token) {
+          window.localStorage.setItem(
+            process.env.REACT_APP_ACCESS_TOKEN_KEY,
+            token
+          );
+          window.localStorage.setItem(
+            process.env.REACT_APP_USER_INFO_KEY,
+            JSON.stringify({ id, username })
+          );
+          message.success('登录成功');
+          setTimeout(() => {
+            navigate(`/user/${id}/profile`);
+          }, 3000);
+        }
+      }
+    });
+  };
 
-      const username = loginFormRef.current.username.value;
-      const password = loginFormRef.current.password.value;
-
-      api
-        .login({
-          username,
-          password,
-        })
-        .then((res) => {
-          if (res.code === 0) {
-            let { token, id, username } = res.data;
-            if (token) {
-              window.localStorage.setItem(
-                process.env.REACT_APP_ACCESS_TOKEN_KEY,
-                token
-              );
-              window.localStorage.setItem(
-                process.env.REACT_APP_USER_INFO_KEY,
-                JSON.stringify({ id, username })
-              );
-              message.success('登录成功');
-              setTimeout(() => {
-                navigate(`/user/${id}/profile`);
-              }, 3000);
-            }
-          }
-        });
-    };
-  }
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 8 },
+  };
 
   return (
     <div className="auth-sign-in-page">
-      <h2 className="title">Login</h2>
-      <form ref={loginFormRef}>
-        <label className="label" htmlFor="username">
-          用户名
-        </label>
-        <input type="text" name="username" id="username"></input>
-        <label className="label" htmlFor="password">
-          密码
-        </label>
-        <input type="password" name="password" id="password"></input>
-
-        <Button className="sign-in" type="primary" onClick={clickHandler()}>
-          登录
-        </Button>
-      </form>
+      <h2 className="title">用户登录</h2>
+      <Form form={form} {...layout} onFinish={handleFinish}>
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名',
+            },
+          ]}
+        >
+          <Input maxLength={14} />
+        </Form.Item>
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码',
+            },
+          ]}
+        >
+          <Input.Password maxLength={14} />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
