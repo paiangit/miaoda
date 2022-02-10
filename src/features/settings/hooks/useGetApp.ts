ad5ba148974b81be4e7b829561dbbefdbf1aaf83
paiangit <pai_an@outlook.com>
2022-02-10 16:08:22 +0800

    chore: 当id为空的时候，不发起react-query请求

--- src/features/settings/hooks/useGetApp.ts
+++ src/features/settings/hooks/useGetApp.ts
@@ -12,7 +12,13 @@ export const getApp = async (id: number) => {
 };
 
 export function useGetApp(id: number) {
-  return useQuery(['getApp', id], async () => {
-    return await getApp(id);
-  });
+  return useQuery(
+    ['getApp', id],
+    async () => {
+      return await getApp(id);
+    },
+    {
+      enabled: !!id, // 表示当id为空的时候，就不要发起请求了
+    }
+  );
 }
