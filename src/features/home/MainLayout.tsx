636e243a4e9f38e5e227c21afcc4cbf96c081e9e
paian <pai_an@outlook.com>
2022-02-16 03:24:44 +0800

    chore: 修改页面导航栏和布局

--- src/features/home/MainLayout.tsx
+++ src/features/home/MainLayout.tsx
@@ -1,44 +1,97 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 import './MainLayout.less';
 
 function MainLayout() {
+  const { Header, Content, Footer } = Layout;
+
+  const menu = [
+    {
+      name: '首页',
+      to: '/',
+      key: 'menu1',
+    },
+    {
+      name: '我的应用',
+      to: '/myApps',
+      key: 'menu2',
+    },
+    {
+      name: 'Management app 1 page 123',
+      to: '/app/1/admin/123',
+      key: 'menu3',
+    },
+    {
+      name: 'Publish app 1',
+      to: '/app/1/admin/appPublish',
+      key: 'menu4',
+    },
+    {
+      name: 'Settings app 1',
+      to: '/app/1/admin/appSettings',
+      key: 'menu5',
+    },
+    {
+      name: 'Design app 1 page 123',
+      to: '/app/1/design?pageId=123',
+      key: 'menu6',
+    },
+    {
+      name: 'Preview app 1 page 123',
+      to: '/app/1/preview?pageId=123',
+      key: 'menu7',
+    },
+    {
+      name: '注册',
+      to: '/auth/register',
+      key: 'menu8',
+    },
+    {
+      name: '登录',
+      to: '/auth/login',
+      key: 'menu9',
+    },
+    {
+      name: '我的档案',
+      to: '/user/1/profile',
+      key: 'menu10',
+    },
+    // {
+    //   name: '计数器例子',
+    //   to: '/examples/counter',
+    //   key: 'menu11',
+    // },
+  ];
+
+  const generateMenu = () => menu.map(item => {
+    return (
+      <Menu.Item key={item.key}>
+        <Link to={item.to}>
+          {item.name}
+        </Link>
+      </Menu.Item>
+    )
+  });
+
   return (
     <div className="home-main-layout">
-      <ul className="demo-nav">
-        <li>
-          <Link to="/">首页</Link>
-        </li>
-        <li>
-          <Link to="/myApps">我的应用</Link>
-        </li>
-        <li>
-          <Link to="/app/1/admin/123">Page Management for app 1 page 123</Link>
-        </li>
-        <li>
-          <Link to="/app/1/admin/appPublish">Publish app 1</Link>
-        </li>
-        {/* <li><Link to="/app/1/admin/appSettings">Settings for app 1</Link></li> */}
-        <li>
-          <Link to="/app/1/design?pageId=123">Design app 1 page 123</Link>
-        </li>
-        <li>
-          <Link to="/app/1/preview?pageId=123">Preview app 1 page 123</Link>
-        </li>
-        <li>
-          <Link to="/auth/register">注册</Link>
-        </li>
-        <li>
-          <Link to="/auth/login">登录</Link>
-        </li>
-        <li>
-          <Link to="/user/1/profile">我的档案</Link>
-        </li>
-        <li>
-          <Link to="/examples/counter">计数器例子</Link>
-        </li>
-      </ul>
-      <Outlet />
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            { generateMenu() }
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Layout>
+            <Content>
+              <Outlet />
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Miao Da ©2022 by Paian</Footer>
+      </Layout>
     </div>
   );
 }
--- src/features/management/AdminLayout.tsx
+++ src/features/management/AdminLayout.tsx
@@ -1,15 +1,60 @@
-import { Button } from 'antd';
+import { Layout, Menu, Button } from 'antd';
 import { Outlet } from 'react-router-dom';
 import './AdminLayout.less';
 
 export default function AdminLayout() {
+  const { Header, Content, Footer, Sider } = Layout;
+  const { SubMenu } = Menu;
+
   const handleClick = () => {
     window.history.go(-1);
   };
 
   return (
     <div className="management-admin-layout">
-      <div>Admin layout</div>
+      {/* <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" title="subnav 1">
+                  <Menu.Item key="1">option1</Menu.Item>
+                  <Menu.Item key="2">option2</Menu.Item>
+                  <Menu.Item key="3">option3</Menu.Item>
+                  <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" title="subnav 2">
+                  <Menu.Item key="5">option5</Menu.Item>
+                  <Menu.Item key="6">option6</Menu.Item>
+                  <Menu.Item key="7">option7</Menu.Item>
+                  <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" title="subnav 3">
+                  <Menu.Item key="9">option9</Menu.Item>
+                  <Menu.Item key="10">option10</Menu.Item>
+                  <Menu.Item key="11">option11</Menu.Item>
+                  <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Miao Da ©2022 by Paian</Footer>
+      </Layout> */}
       <Outlet />
       <br />
       <Button type="link" onClick={handleClick}>返回</Button>
