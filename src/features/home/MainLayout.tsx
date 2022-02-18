c7ac446da5bbf7e0c2993a1a9eef8b6c01d41a28
paian <pai_an@outlook.com>
2022-02-18 13:13:36 +0800

    fix: 修复未登录时的报错

--- src/features/home/MainLayout.tsx
+++ src/features/home/MainLayout.tsx
@@ -1,76 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import './MainLayout.less';
 
 function MainLayout() {
   const { Header, Content, Footer } = Layout;
   const [menu, setMenu] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const userInfo = window.localStorage.getItem(process.env.REACT_APP_USER_INFO_KEY);
-    const userId = JSON.parse(userInfo).id;
+    try {
+      const userInfo = window.localStorage.getItem(process.env.REACT_APP_USER_INFO_KEY);
+      // 未登录时userInfo为null，会报错
+      const userId = JSON.parse(userInfo).id;
 
-    const data = [
-      {
-        name: '首页',
-        to: '/',
-        key: 'menu1',
-      },
-      {
-        name: '我的应用',
-        to: `/user/${userId}/myApps`,
-        key: 'menu2',
-      },
-      {
-        name: '我的档案',
-        to: `/user/${userId}/profile`,
-        key: 'menu10',
-      },
-      // {
-      //   name: 'Management app 1 page 123',
-      //   to: '/app/1/admin/123',
-      //   key: 'menu3',
-      // },
-      // {
-      //   name: 'Publish app 1',
-      //   to: '/app/1/admin/appPublish',
-      //   key: 'menu4',
-      // },
-      // {
-      //   name: 'Settings app 1',
-      //   to: '/app/1/admin/appSettings',
-      //   key: 'menu5',
-      // },
-      // {
-      //   name: 'Design app 1 page 123',
-      //   to: '/app/1/design/123',
-      //   key: 'menu6',
-      // },
-      // {
-      //   name: 'Preview app 1 page 123',
-      //   to: '/app/1/preview/123',
-      //   key: 'menu7',
-      // },
-      {
-        name: '注册',
-        to: '/auth/register',
-        key: 'menu8',
-      },
-      {
-        name: '登录',
-        to: '/auth/login',
-        key: 'menu9',
-      },
-      // {
-      //   name: 'Todo List',
-      //   to: '/examples/todos',
-      //   key: 'menu11',
-      // },
-    ];
+      const data = [
+        {
+          name: '首页',
+          to: '/',
+          key: 'menu1',
+        },
+        {
+          name: '我的应用',
+          to: `/user/${userId}/myApps`,
+          key: 'menu2',
+        },
+        {
+          name: '我的档案',
+          to: `/user/${userId}/profile`,
+          key: 'menu10',
+        },
+        // {
+        //   name: 'Management app 1 page 123',
+        //   to: '/app/1/admin/123',
+        //   key: 'menu3',
+        // },
+        // {
+        //   name: 'Publish app 1',
+        //   to: '/app/1/admin/appPublish',
+        //   key: 'menu4',
+        // },
+        // {
+        //   name: 'Settings app 1',
+        //   to: '/app/1/admin/appSettings',
+        //   key: 'menu5',
+        // },
+        // {
+        //   name: 'Design app 1 page 123',
+        //   to: '/app/1/design/123',
+        //   key: 'menu6',
+        // },
+        // {
+        //   name: 'Preview app 1 page 123',
+        //   to: '/app/1/preview/123',
+        //   key: 'menu7',
+        // },
+        {
+          name: '注册',
+          to: '/auth/register',
+          key: 'menu8',
+        },
+        {
+          name: '登录',
+          to: '/auth/login',
+          key: 'menu9',
+        },
+        // {
+        //   name: 'Todo List',
+        //   to: '/examples/todos',
+        //   key: 'menu11',
+        // },
+      ];
 
-    setMenu(data);
-  }, [])
+      setMenu(data);
+    } catch(err) {
+      navigate('/auth/login')
+    }
+  }, [navigate])
 
   const generateMenu = () => menu.map(item => {
     return (
