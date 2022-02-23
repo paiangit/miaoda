9b5256f28ece19fb61c2b207eb324cd261306a24
paian <pai_an@outlook.com>
2022-02-23 18:42:11 +0800

    chore: 删除无用代码

--- src/features/examples/TodosPage.tsx
+++ src/features/examples/TodosPage.tsx
@@ -1,19 +1,12 @@
-import React, { useCallback, useState } from 'react';
 import useDocumentTitle from '~hooks/useDocumentTitle';
 import TodoList from './TodoList';
 import './TodosPage.less';
 
 export default function TodosPage() {
   useDocumentTitle('例子：Todo List');
-  const [, setValue] = useState('');
-
-  const handleChange = useCallback((e) => {
-    setValue(e.target.value);
-  }, [setValue]);
 
   return (
     <div className="examples-todos-page">
-      <input onChange={handleChange}></input>
       <TodoList></TodoList>
     </div>
   );
